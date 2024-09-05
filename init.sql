3a45979a4f2a0478b7d454f3bde9eeca5f2aa71a
xbmlz <997909544@qq.com>
2024-09-05 18:08:30 +0800

    feat: add sql

--- init.sql
+++ init.sql
@@ -65,22 +65,96 @@ INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "ver
 INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (58, '苹果', 'iPhone 15系列', 'iPhone 15', '黄色', '512GB');
 
 
--- 华为Pura 70 Pro+
-INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (59, '华为', 'Pura系列', '华为Pura 70 Pro+', '弦乐白', '16+512GB');
-INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (60, '华为', 'Pura系列', '华为Pura 70 Pro+', '弦乐白', '16+1TB');
-INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (61, '华为', 'Pura系列', '华为Pura 70 Pro+', '光织银', '16+512GB');
-INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (62, '华为', 'Pura系列', '华为Pura 70 Pro+', '光织银', '16+1TB');
-INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (63, '华为', 'Pura系列', '华为Pura 70 Pro+', '魅影黑', '16+512GB');
-INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (64, '华为', 'Pura系列', '华为Pura 70 Pro+', '魅影黑', '16+1TB');
+-- Pura 70 Pro+
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (59, '华为', 'Pura系列', 'Pura 70 Pro+', '弦乐白', '16+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (60, '华为', 'Pura系列', 'Pura 70 Pro+', '弦乐白', '16+1TB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (61, '华为', 'Pura系列', 'Pura 70 Pro+', '光织银', '16+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (62, '华为', 'Pura系列', 'Pura 70 Pro+', '光织银', '16+1TB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (63, '华为', 'Pura系列', 'Pura 70 Pro+', '魅影黑', '16+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (64, '华为', 'Pura系列', 'Pura 70 Pro+', '魅影黑', '16+1TB');
 
--- 华为Pura 70 Pro 雪域白 罗兰紫 羽砂黑 | 12GB+256GB 12GB+512GB 12GB+1TB
-INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (65, '华为', 'Pura系列', '华为Pura 70 Pro', '雪域白', '12GB+256GB');
-INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (66, '华为', 'Pura系列', '华为Pura 70 Pro', '雪域白', '12GB+512GB');
-INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (67, '华为', 'Pura系列', '华为Pura 70 Pro', '雪域白', '12GB+1TB');
-INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (68, '华为', 'Pura系列', '华为Pura 70 Pro', '罗兰紫', '12GB+256GB');
-INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (69, '华为', 'Pura系列', '华为Pura 70 Pro', '罗兰紫', '12GB+512GB');
-INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (70, '华为', 'Pura系列', '华为Pura 70 Pro', '罗兰紫', '12GB+1TB');
+-- Pura 70 Pro 雪域白 罗兰紫 羽砂黑 | 12GB+256GB 12GB+512GB 12GB+1TB
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (65, '华为', 'Pura系列', 'Pura 70 Pro', '雪域白', '12GB+256GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (66, '华为', 'Pura系列', 'Pura 70 Pro', '雪域白', '12GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (67, '华为', 'Pura系列', 'Pura 70 Pro', '雪域白', '12GB+1TB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (68, '华为', 'Pura系列', 'Pura 70 Pro', '罗兰紫', '12GB+256GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (69, '华为', 'Pura系列', 'Pura 70 Pro', '罗兰紫', '12GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (70, '华为', 'Pura系列', 'Pura 70 Pro', '罗兰紫', '12GB+1TB');
 
--- 华为Pura 70 Ultra  颜色 香颂绿 星芒黑 星芒白 摩卡棕 版本 16GB+1TB 16GB+512GB
-INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (71, '华为', 'Pura系列', '华为Pura 70 Ultra', '香颂绿', '16GB+512GB');
-INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (72, '华为', 'Pura系列', '华为Pura 70 Ultra', '香颂绿', '16GB+1TB');
+-- Pura 70 Ultra  颜色 香颂绿 星芒黑 星芒白 摩卡棕 版本 16GB+1TB 16GB+512GB
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (71, '华为', 'Pura系列', 'Pura 70 Ultra', '香颂绿', '16GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (72, '华为', 'Pura系列', 'Pura 70 Ultra', '香颂绿', '16GB+1TB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (73, '华为', 'Pura系列', 'Pura 70 Ultra', '星芒黑', '16GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (74, '华为', 'Pura系列', 'Pura 70 Ultra', '星芒黑', '16GB+1TB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (75, '华为', 'Pura系列', 'Pura 70 Ultra', '星芒白', '16GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (76, '华为', 'Pura系列', 'Pura 70 Ultra', '星芒白', '16GB+1TB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (77, '华为', 'Pura系列', 'Pura 70 Ultra', '摩卡棕', '16GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (78, '华为', 'Pura系列', 'Pura 70 Ultra', '摩卡棕', '16GB+1TB');
+
+-- Pura 70 颜色： 冰晶蓝 羽砂黑 雪域白 樱玫红 12GB+1TB 12GB+512GB 12GB+256GB
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (79, '华为', 'Pura系列', 'Pura 70', '冰晶蓝', '12GB+256GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (80, '华为', 'Pura系列', 'Pura 70', '冰晶蓝', '12GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (81, '华为', 'Pura系列', 'Pura 70', '冰晶蓝', '12GB+1TB');
+
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (82, '华为', 'Pura系列', 'Pura 70', '羽砂黑', '12GB+256GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (83, '华为', 'Pura系列', 'Pura 70', '羽砂黑', '12GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (84, '华为', 'Pura系列', 'Pura 70', '羽砂黑', '12GB+1TB');
+
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (85, '华为', 'Pura系列', 'Pura 70', '雪域白', '12GB+256GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (86, '华为', 'Pura系列', 'Pura 70', '雪域白', '12GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (87, '华为', 'Pura系列', 'Pura 70', '雪域白', '12GB+1TB');
+
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (88, '华为', 'Pura系列', 'Pura 70', '樱玫红', '12GB+256GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (89, '华为', 'Pura系列', 'Pura 70', '樱玫红', '12GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (90, '华为', 'Pura系列', 'Pura 70', '樱玫红', '12GB+1TB');
+
+-- 华为Mate 60 Pro+ 砚黑 宣白  16GB+512GB 16GB+1TB
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (91, '华为', 'Mate系列', 'Mate 60 Pro+', '砚黑', '16GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (92, '华为', 'Mate系列', 'Mate 60 Pro+', '砚黑', '16GB+1TB');
+
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (93, '华为', 'Mate系列', 'Mate 60 Pro+', '宣白', '16GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (94, '华为', 'Mate系列', 'Mate 60 Pro+', '宣白', '16GB+1TB');
+
+-- 华为Mate 60 Pro 雅川青 白沙银 南糯紫 雅丹黑 12GB+256GB 12GB+512GB 12GB+1TB
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (95, '华为', 'Mate系列', 'Mate 60 Pro', '雅川青', '12GB+256GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (96, '华为', 'Mate系列', 'Mate 60 Pro', '雅川青', '12GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (97, '华为', 'Mate系列', 'Mate 60 Pro', '雅川青', '12GB+1TB');
+
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (98, '华为', 'Mate系列', 'Mate 60 Pro', '白沙银', '12GB+256GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (99, '华为', 'Mate系列', 'Mate 60 Pro', '白沙银', '12GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (100, '华为', 'Mate系列', 'Mate 60 Pro', '白沙银', '12GB+1TB');
+
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (101, '华为', 'Mate系列', 'Mate 60 Pro', '南糯紫', '12GB+256GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (102, '华为', 'Mate系列', 'Mate 60 Pro', '南糯紫', '12GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (103, '华为', 'Mate系列', 'Mate 60 Pro', '南糯紫', '12GB+1TB');
+
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (104, '华为', 'Mate系列', 'Mate 60 Pro', '雅丹黑', '12GB+256GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (105, '华为', 'Mate系列', 'Mate 60 Pro', '雅丹黑', '12GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (106, '华为', 'Mate系列', 'Mate 60 Pro', '雅丹黑', '12GB+1TB');
+
+-- 华为Mate X5 羽砂黑 羽砂白 羽砂金 青山黛 幻影紫 12GB+512GB 16GB+512GB 16GB+512GB典藏版 16GB+1TB典藏版
+
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (107, '华为', 'Mate系列', 'Mate X5', '羽砂黑', '12GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (108, '华为', 'Mate系列', 'Mate X5', '羽砂黑', '16GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (109, '华为', 'Mate系列', 'Mate X5', '羽砂黑', '16GB+512GB典藏版');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (110, '华为', 'Mate系列', 'Mate X5', '羽砂黑', '16GB+1TB典藏版');
+
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (111, '华为', 'Mate系列', 'Mate X5', '羽砂白', '12GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (112, '华为', 'Mate系列', 'Mate X5', '羽砂白', '16GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (113, '华为', 'Mate系列', 'Mate X5', '羽砂白', '16GB+512GB典藏版');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (114, '华为', 'Mate系列', 'Mate X5', '羽砂白', '16GB+1TB典藏版');
+
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (115, '华为', 'Mate系列', 'Mate X5', '羽砂金', '12GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (116, '华为', 'Mate系列', 'Mate X5', '羽砂金', '16GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (117, '华为', 'Mate系列', 'Mate X5', '羽砂金', '16GB+512GB典藏版');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (118, '华为', 'Mate系列', 'Mate X5', '羽砂金', '16GB+1TB典藏版');
+
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (119, '华为', 'Mate系列', 'Mate X5', '青山黛', '12GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (120, '华为', 'Mate系列', 'Mate X5', '青山黛', '16GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (121, '华为', 'Mate系列', 'Mate X5', '青山黛', '16GB+512GB典藏版');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (122, '华为', 'Mate系列', 'Mate X5', '青山黛', '16GB+1TB典藏版');
+
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (123, '华为', 'Mate系列', 'Mate X5', '幻影紫', '12GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (124, '华为', 'Mate系列', 'Mate X5', '幻影紫', '16GB+512GB');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (125, '华为', 'Mate系列', 'Mate X5', '幻影紫', '16GB+512GB典藏版');
+INSERT INTO "public"."products" ("id", "brand", "series", "model", "color", "version") VALUES (126, '华为', 'Mate系列', 'Mate X5', '幻影紫', '16GB+1TB典藏版');

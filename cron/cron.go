ba3f89e1000f49b114643d6fd53d5e63bdbd882c
xbmlz <997909544@qq.com>
2024-09-14 10:22:16 +0800

    feat: add cron

--- cron/cron.go
+++ cron/cron.go
@@ -13,43 +13,53 @@ func Run() {
 	c := cron.New()
 	c.AddFunc("0 10,14,18,22 * * *", func() {
 		log.Println("wx notify cron job start")
-
-		configGroups, err := model.GetConfigByKey("cron:wx-notify-groups")
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		configContent, err := model.GetConfigByKey("cron:wx-notify-content")
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		g, err := wechat.Self.Groups()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		if configGroups.ConfigVal == "" || configContent.ConfigVal == "" {
-			log.Println("config is empty")
-			return
-		}
-
-		toGroups := strings.Split(configGroups.ConfigVal, ",")
-
-		for _, toGroup := range toGroups {
-			for _, group := range g {
-				if group.NickName == toGroup {
-					log.Println("send message to " + toGroup)
-					group.SendText(configContent.ConfigVal)
-				}
-			}
-		}
-
+		sendWxMsg(`
+https://bj.xbmlz.cc/
+最新报价网站 华为参考价格 苹果参考价格比较准 
+本消息由报价机器人发送
+		`)
 		log.Println("wx notify cron job end")
+	})
 
+	c.AddFunc("0 0 11,17,21 * *?", func() {
+		log.Println("wx notify cron job start")
+		sendWxMsg(`
+今天明天到货提前联系群主啦
+中秋不放假
+机器人发送 
+		`)
+		log.Println("wx notify cron job end")
 	})
 	c.Start()
+}
+
+func sendWxMsg(msg string) {
+
+	g, err := wechat.Self.Groups()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	configGroups, err := model.GetConfigByKey("cron:wx-notify-groups")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	if configGroups.ConfigVal == "" {
+		log.Println("config is empty")
+		return
+	}
+
+	toGroups := strings.Split(configGroups.ConfigVal, ",")
+
+	for _, toGroup := range toGroups {
+		for _, group := range g {
+			if group.NickName == toGroup {
+				log.Println("send message to " + toGroup)
+				group.SendText(msg)
+			}
+		}
+	}
 }
